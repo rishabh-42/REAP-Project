947aac09c4894c7c82b71bbe676cf884e9c65914
Rishabh Rajput <rishabh.rajput@tothenew.com>
2019-04-06 22:53:08 +0530

    added revoke feature 1/2 done

--- src/main/java/com/example/spring/Controller/RevokeRecognitionController.java
+++ src/main/java/com/example/spring/Controller/RevokeRecognitionController.java
@@ -1,9 +1,27 @@
 package com.example.spring.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class RevokeRecognitionController {
 
+    @PostMapping("/revoke")
+    @ResponseBody
+    public String revokeRecognition(@RequestParam Map<String,String> postData){
 
+
+        System.out.println(postData);
+        try {
+            Thread.sleep(6000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "done";
+
+    }
 }
