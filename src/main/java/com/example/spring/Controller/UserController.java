de3e79f66c51c8da775978cd1b47c72444b7a668
Rishabh Rajput <rishabh.rajput@tothenew.com>
2019-04-08 14:04:39 +0530

    manage roles added

--- src/main/java/com/example/spring/Controller/UserController.java
+++ src/main/java/com/example/spring/Controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.spring.Controller;
 
 import com.example.spring.Entities.User;
+import com.example.spring.Entities.UserRole;
 import com.example.spring.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -16,6 +19,8 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+
 
     @RequestMapping("/getAllUsers")
     List<User> get(){
@@ -28,4 +33,14 @@ public class UserController {
         userList=userList.stream().filter(e->!e.getEmail().equals(userName)).collect(Collectors.toList());
         return userList;
     }
+
+    @RequestMapping("/getUserRoles")
+    Set<UserRole> getRoles(@RequestParam("email") String email){
+
+        Set<UserRole> userRoles= userService.findByEmail(email).getRoles();
+
+
+        return userRoles;
+    }
+
 }
