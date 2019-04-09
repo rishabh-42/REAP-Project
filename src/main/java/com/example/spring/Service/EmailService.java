3644e90377129ccfcf57494dd9a6fb687886a948
Rishabh Rajput <rishabh.rajput@tothenew.com>
2019-04-09 14:44:01 +0530

    few changes : admin download csv added

--- src/main/java/com/example/spring/Service/EmailService.java
+++ src/main/java/com/example/spring/Service/EmailService.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
+
+@EnableAsync
 @Service("emailService")
-public class EmailService {
+public class EmailService  {
 
     private JavaMailSender mailSender;
 
